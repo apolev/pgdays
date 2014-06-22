e72f71f73e896abfe047cd47cf69b302eaa91e6d
apolev <alex.v.polev@gmail.com>
2014-06-22 10:37:21 +0400

    Добавление и редактирование отделов. Часть кода по добавлению сотрудников.

--- database/procedures.sql
+++ database/procedures.sql
@@ -12,7 +12,7 @@ $body$
   END;
 $body$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION "get_departments" ("limit" integer default null)
+CREATE OR REPLACE FUNCTION "get_departments" ("lookup_id" integer default null, "limit" integer default null)
   RETURNS SETOF "departments" AS
   $body$
   BEGIN
@@ -20,8 +20,54 @@ CREATE OR REPLACE FUNCTION "get_departments" ("limit" integer default null)
       SELECT *
       FROM departments
       WHERE NOT is_deleted
+        AND (lookup_id IS NULL OR id = lookup_id)
+      ORDER BY id
       LIMIT "limit"
     ;
+  END;
+$body$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION "get_department_employees" ("lookup_id" integer)
+  RETURNS SETOF "employees" AS
+  $body$
+  BEGIN
+    RETURN QUERY
+      SELECT e.*
+      FROM departments_employees AS de
+        INNER JOIN employees AS e
+          ON de.employee_id = e.id
+      WHERE de.department_id = lookup_id
+        AND NOT e.is_deleted
+      ;
+  END;
+$body$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION "get_available_for_adding_employees" ("lookup_id" integer)
+  RETURNS SETOF "employees" AS
+  $body$
+  BEGIN
+    RETURN QUERY
+    WITH current_employee_ids AS (
+      SELECT de.employee_id
+      FROM departments_employees AS de
+      WHERE de.department_id = lookup_id
+    )
+    SELECT e.*
+    FROM employees AS e
+    WHERE e.id NOT IN (SELECT employee_id FROM current_employee_ids)
+      AND NOT e.is_deleted
+    ;
+  END;
+$body$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION "edit_department" (lookup_id integer, new_title varchar)
+  RETURNS integer AS
+  $body$
+  BEGIN
+    UPDATE departments
+    SET title = new_title
+    WHERE id = lookup_id;
+    RETURN NULL;
   END;
 $body$ LANGUAGE plpgsql;
 
--- src/Controller/DepartmentsController.php
+++ src/Controller/DepartmentsController.php
@@ -4,7 +4,8 @@ namespace Controller;
 
 use Bravicility\Http\Request;
 use Bravicility\Http\Response\HtmlResponse;
-use Bravicility\Http\Response\Response;
+use Bravicility\Http\Response\RedirectResponse;
+use Bravicility\Router\RouteNotFoundException;
 
 class DepartmentsController extends ControllerAbstract
 {
@@ -16,21 +17,88 @@ class DepartmentsController extends ControllerAbstract
         $db          = $this->container->getDb();
         $departments = $db->execute('SELECT * FROM get_departments()')->fetchAll();
 
-        return new HtmlResponse(200, $this->twig->render('departments.twig', array('departments' => $departments)));
+        return new HtmlResponse(200, $this->twig->render('departments.twig', ['departments' => $departments]));
+    }
+
+    /**
+     * @route POST /departments/add
+     */
+    public function doAdd(Request $request)
+    {
+        $title = $request->post('title');
+        $this->container->getDb()->execute('SELECT add_department(?q)', [$title]);
+
+        return new RedirectResponse('/departments');
     }
 
     /**
      * @route GET /departments/edit/
      */
-    public function get(Request $request)
+    public function edit(Request $request)
+    {
+        $department = $this->getDepartment($request->get('id'));
+
+        return new HtmlResponse(200, $this->twig->render(
+            'departments.edit.twig',
+            [
+                'department'         => $department['data'],
+                'availableEmployees' => $department['availableEmployees'],
+                'currentEmployees'   => $department['currentEmployees'],
+            ]));
+    }
+
+    /**
+     * @route POST /departments/edit/
+     */
+    public function doEdit(Request $request)
+    {
+        $id         = $request->post('id');
+        $title      = $request->post('title');
+        $department = $this->getDepartment($id);
+        $db         = $this->container->getDb();
+
+        $db->execute('SELECT edit_department(?q, ?q)', [$department['id'], $title]);
+
+        return new RedirectResponse('/departments');
+    }
+
+    /**
+     * @route POST /departments/employees/
+     */
+    public function doAddEmployee(Request $request)
+    {
+
+    }
+
+    /**
+     * return array
+     */
+    private function getDepartment($id)
     {
         $db         = $this->container->getDb();
-        $department = $db->execute('SELECT * FROM get_departments(?q)', array($request->get('id')))->fetchOneOrFalse();
+        $department = $db->execute(
+            'SELECT * FROM get_departments(?q)',
+            [$id]
+        )->fetchOneOrFalse();
 
-        if ($department) {
-            return new Response(404, 'Отдел не найден.');
+        if (!$department) {
+            throw new RouteNotFoundException();
         }
 
-        return new HtmlResponse(200, $this->twig->render('departments.twig', array('department' => $department)));
+        $currentEmployees = $db->execute(
+            'SELECT * FROM get_department_employees(?q)',
+            [$id]
+        )->fetchAll();
+
+        $availableEmployees = $db->execute(
+            'SELECT * FROM get_available_for_adding_employees(?q)',
+            [$id]
+        )->fetchAll();
+
+        return [
+            'data'               => $department,
+            'currentEmployees'   => $currentEmployees,
+            'availableEmployees' => $availableEmployees
+        ];
     }
 }
